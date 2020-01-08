eecfd1a7d106e4f1bf2e5a31fe271f88e65a093a
Tim <tim.lyshchenko@gmail.com>
2020-01-08 17:02:44 +0200

    Installed client template with create-react-app. Updated package.json commands

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      Edit <code>src/App.js</code> and save to reload.
+    </div>
+  );
+}
+
+export default App;
